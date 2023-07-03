d1f0dfa88ef203de74843be2bab328ab76c3d6b0
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-07-03 17:38:18 +0500

    try rtk

--- apps/schools/http/api/index.ts
+++ apps/schools/http/api/index.ts
@@ -6,7 +6,7 @@ export const commonApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: BASE_API_URL,
         prepareHeaders: headers => {
-            headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iis3OTk5OTk5OTk5OSIsImlhdCI6MTY4ODEyNDcwMywiZXhwIjoxNjg4NzI5NTAzLCJqdGkiOiJlMWU0Mzg1MC1lNDI3LTQ0NzUtOTM3MS1hYzdhNmEzZmE1MGMiLCJ1c2VyX2lkIjoiZGM3M2RkNzQtYmY0Zi00NDgyLTg5ZjAtMzYzMWEyZDQ0MzIwIn0.HcMoJQNa6aYUkwm-5gDdrQGq8YAcZuiYOi_U5B-F5BE')
+            headers.set('Authorization', 'Bearer token')
             headers.set('Content-Type', 'application/json;charset=UTF-8')
             return headers
         },
