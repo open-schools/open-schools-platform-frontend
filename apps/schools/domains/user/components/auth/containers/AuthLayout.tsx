d4d497d9aea13255272a1739e64534e52560c930
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-07-14 15:35:26 +0500

    fix

--- apps/schools/domains/user/components/auth/containers/AuthLayout.tsx
+++ apps/schools/domains/user/components/auth/containers/AuthLayout.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import styles from './styles/styles.module.scss'
+import Link from 'next/link'
+import { Row } from 'antd'
+import logo from '@public/icons/logo.svg'
+import Image from 'next/image'
 
 export interface IAuthLayoutProps {
     headerAction: React.ReactElement,
@@ -12,8 +16,25 @@ const AuthLayout: React.FC<IAuthLayoutProps> = (props) => {
     return (
         <div
             className={styles.container}
+            style={{ backgroundImage: 'url(/image/authImage.svg)' }}
         >
+            {/*# TODO: add env for link there */}
+            <Link className={styles.logoContainer} href={'https://openschools.education'}>
+                <Row className={styles.rowWithGap}>
+                    <Image src={logo} alt={'Логотип'} width={50}></Image>
+                    <div className={styles.logoText}>
+                        Открытые<br/>
+                        школы
+                    </div>
+                </Row>
+            </Link>
             {children}
+            {/*# TODO: add env for email there */}
+            <Link className={styles.emailContainer} href={'mailto:inbox@openschools.education'}>
+                <div className={styles.emailText}>
+                    inbox@openschools.education
+                </div>
+            </Link>
         </div>
     )
 }
