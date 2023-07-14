95ec4a47d905c00b9f73e32a713915c2d719d32d
zavarin-michael <miha061103@gmail.com>
2023-07-14 16:13:44 +0500

    added env variables in docker compose files

--- apps/schools/setEnv.sh
+++ apps/schools/setEnv.sh
@@ -2,4 +2,11 @@
 
 cat <<EOT >> .env.production
 NEXT_PUBLIC_BASE_URL="$NEXT_PUBLIC_BASE_URL"
+NEXT_PUBLIC_apiKey="$NEXT_PUBLIC_apiKey"
+NEXT_PUBLIC_authDomain="$NEXT_PUBLIC_authDomain"
+NEXT_PUBLIC_projectId="$NEXT_PUBLIC_projectId"
+NEXT_PUBLIC_storageBucket="$NEXT_PUBLIC_storageBucket"
+NEXT_PUBLIC_messagingSenderId="$NEXT_PUBLIC_messagingSenderId"
+NEXT_PUBLIC_appId="$NEXT_PUBLIC_appId"
+NEXT_PUBLIC_measurementId="$NEXT_PUBLIC_measurementId"
 EOT
