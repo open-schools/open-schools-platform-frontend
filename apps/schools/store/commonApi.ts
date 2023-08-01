b6b191a85db815a11552233f2a2b89d1be710550
lev tarasov <Levil6.6.4@yandex.ru>
2023-08-01 11:49:43 +0500

    del util, errorDict in commonApi

--- apps/schools/store/commonApi.ts
+++ apps/schools/store/commonApi.ts
@@ -2,8 +2,36 @@ import Cookies from 'universal-cookie'
 import { BaseQueryFn, createApi, FetchArgs, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 import { isRejected, Middleware, MiddlewareAPI } from '@reduxjs/toolkit'
 import { message } from 'antd'
-import { errorCodes, ErrorType } from './utils'
 
+
+const errorCodes = {
+    400: {
+        ValidationError: 'Ошибка валидации данных',
+        ParseError: 'Ошибка разбора запроса',
+        InvalidArgument: 'Некорректный аргумент',
+        QueryCorrupted: 'Объект поврежден',
+        WrongStatusChange: 'Неверное изменение статуса',
+        EmailServiceUnavailable: 'Сервис отправки электронной почты недоступен',
+        AlreadyExists: 'Объект с такими параметрами уже существует',
+        MapServiceUnavailable: 'Сервис карт недоступен',
+    },
+    401: 'Вы не аутентифицированы. Пожалуйста, выполните вход для доступа к запрашиваемому ресурсу',
+    403: 'Недостаточно прав для выполнения операции. Обратитесь к администратору для получения необходимых разрешений',
+    404: 'Объект не найден. Пожалуйста, убедитесь, что указанный ресурс существует и повторите запрос',
+    429: 'Слишком много попыток. Пожалуйста, подождите некоторое время и повторите запрос позже',
+}
+
+interface ErrorType {
+    data: {
+        error: {
+            code: string,
+            message?: string,
+            'violation_fields': any,
+            violations: [string]
+        }
+    },
+    status: number,
+}
 
 const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_URL
 const cookies = new Cookies()
@@ -34,15 +62,9 @@ export const rtkQueryErrorLogger: Middleware = (api: MiddlewareAPI) => next => a
         const { status, data } = action.payload
         const errorData = data && data.error
         const errorType = errorCodes[status]
-        console.log(errorData)
-        console.log(data)
-        console.log(action)
 
-        if (typeof errorType === 'string') {
-            message.error(errorType);
-        } else if (errorData.code) {
-            message.error(errorType[errorData.code])
-        }
+        typeof errorType === 'string' ? message.error(errorType)
+            : errorData?.code && message.error(errorType?.[errorData.code])
     }
     return next(action)
 }
