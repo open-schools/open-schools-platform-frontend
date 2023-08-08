d52c52596bbd322c613d90f72bed8af577539421
lev tarasov <Levil6.6.4@yandex.ru>
2023-08-08 20:10:28 +0500

    feature: search and highlight search text

---
+++ apps/schools/domains/common/components/table/forming.tsx
@@ -0,0 +1,83 @@
+import { ColumnType } from 'antd/lib/table/interface'
+import React from 'react'
+
+export interface RawColumnType<DataType> extends ColumnType<DataType> {
+    hidden?: boolean
+    dataIndex: string
+}
+
+export function useGenerateFullColumns<DataType>(baseColumns: RawColumnType<DataType>[]): ColumnType<DataType>[] {
+    return baseColumns.map((column) => ({
+        ...column,
+    }))
+}
+
+function getFiniteValue(obj: any, searchFields: Array<String>, searchText: string) {
+    let newObj: any = {}
+    getProp(obj)
+
+    function getProp(o: any) {
+        for (let prop in o) {
+            if (typeof o[prop] === 'object') {
+                getProp(o[prop])
+            } else {
+                searchFields.forEach((item) => {
+                    if (item == prop) {
+                        newObj[prop] = o[prop]
+                    }
+                })
+            }
+        }
+    }
+    return newObj
+}
+
+function escapeRegExp(text: string) {
+    return text.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+}
+
+function HighlightText({ text, searchText }: any) {
+    const isMatch = text.includes(searchText);
+
+    if (isMatch) {
+        const parts = text.split(new RegExp(`(${escapeRegExp(searchText)})`, 'gi'));
+        return (
+          <span>
+                {parts.map((part: any, index: any) => (
+                  part.toLowerCase() === searchText.toLowerCase() ? (
+                    <span key={index} style={{ backgroundColor: 'yellow' }}>
+                            {part}
+                        </span>
+                  ) : (
+                    <span key={index}>
+                            {part}
+                        </span>
+                  )
+                ))}
+            </span>
+        );
+    }
+
+    return <span>{text}</span>;
+}
+
+export function objectReBuilder(data: Array<Object>, searchFields: string[], searchRequestText: string): Array<Object> {
+    let resultArray: Array<Object> = []
+    const searchText = searchRequestText.split(':')[0]
+
+    data.forEach((item: any) => {
+        const newItem: any = {};
+        for (const field of searchFields) {
+            newItem[field] = (
+              <HighlightText
+                text={item[field]}
+                searchText={searchText}
+              />
+            );
+        }
+        resultArray.push(newItem);
+    })
+
+    return resultArray;
+}
+
--- apps/schools/domains/employee/components/employeeList/index.tsx
+++ apps/schools/domains/employee/components/employeeList/index.tsx
@@ -3,11 +3,12 @@ import { Table, Skeleton, Typography } from 'antd'
 import router from 'next/router'
 import styles from './styles/styles.module.scss'
 import { useGetAllEmployeesQuery } from '@domains/employee/redux/employeeApi'
-import { objectReBuilder, RawColumnType, useGenerateFullColumns } from '@domains/common/components/table'
+import { objectReBuilder, RawColumnType, useGenerateFullColumns } from '@domains/common/components/table/forming'
 import { useOrganization } from '@domains/organization/providers/organizationProvider'
 import { Button } from '@domains/common/components/button'
 import { Input } from '@domains/common/components/input'
-import { SearchOutlined } from '@ant-design/icons'
+import { CloseCircleOutlined, SearchOutlined } from '@ant-design/icons'
+import { search } from '@domains/common/constants/Table'
 
 interface DataType {
     id: string
@@ -44,17 +45,24 @@ const baseColumns: RawColumnType<DataType>[] = [
 ].filter((item) => !item.hidden)
 
 export function EmployeeList() {
+    const [isTableLoading, setIsTableLoading] = useState(false)
+    const [inputText, setInputText] = useState('')
+    const [searchRequestText, setSearchRequestText] = useState('')
     const [dataSource, setDataSource] = useState<any[]>([])
     const { organizationId } = useOrganization()
     const searchFields = ['name', 'position', 'phone', 'id']
+
+
     const { data, isLoading } = useGetAllEmployeesQuery({
         organization: organizationId,
+        or_search: `${searchRequestText}:[${search.map((e: string) => e)}]`,
     })
 
     useEffect(() => {
         if (!isLoading && data) {
-            const result = objectReBuilder(data.results, searchFields)
+            const result = objectReBuilder(data.results, searchFields, searchRequestText)
             setDataSource(result)
+            setIsTableLoading(false)
         }
     }, [isLoading, data])
 
@@ -62,50 +70,55 @@ export function EmployeeList() {
 
     return (
         <>
-            {isLoading ? (
-                <Skeleton active />
-            ) : (
-                <>
-                    <div className={styles.header}>
-                        <Typography.Title level={1}>Сотрудники</Typography.Title>
-                        <Button
-                            type='schoolDefault'
-                            block
-                            style={{ width: '14%' }}
-                            className={styles.button}
-                            onClick={() => router.push('/employee/create')}
-                        >
-                            Добавить сотрудника
-                        </Button>
-                    </div>
-                    <Input
-                        customType={'inputSearch'}
-                        placeholder={'Поиск'}
-                        children={
-                            <SearchOutlined
-                                style={{
-                                    fontSize: '150%',
-                                    position: 'absolute',
-                                    right: '30px',
-                                }}
-                            />
+            <>
+                <div className={styles.header}>
+                    <Typography.Title level={1}>Сотрудники</Typography.Title>
+                    <Button
+                        type='schoolDefault'
+                        block
+                        style={{ width: '14%' }}
+                        className={styles.button}
+                        onClick={() => router.push('/employee/create')}
+                    >
+                        Добавить сотрудника
+                    </Button>
+                </div>
+                <Input
+                    onChange={(text) => {
+                        setIsTableLoading(true)
+                        setInputText(text.target.value)
+                        setTimeout(() =>{
+                            setSearchRequestText(text.target.value)
+                        }, 1000)
+                    }}
+                    customType={'inputSearch'}
+                    placeholder={'Поиск'}
+                    value={inputText}
+                    children={
+                        <>
+                            <SearchOutlined className={styles.search} />
+                            <CloseCircleOutlined className={styles.cross} onClick={() => {
+                                setInputText('')
+                                setSearchRequestText('')
+                            }} />
+                        </>
+                    }
+                />
+                <Table
+                    loading={isTableLoading || isLoading}
+                    className={styles.tableContainer}
+                    columns={columns}
+                    dataSource={dataSource}
+                    rowKey={(record) => record.id}
+                    onRow={(element: DataType) => {
+                        return {
+                            onClick: () => {
+                                router.push(`/employee/${element.id}`)
+                            },
                         }
-                    />
-                    <Table
-                        className={styles.tableContainer}
-                        columns={columns}
-                        dataSource={dataSource}
-                        rowKey={(record) => record.id}
-                        onRow={(element: DataType) => {
-                            return {
-                                onClick: () => {
-                                    router.push(`/employee/${element.id}`)
-                                },
-                            }
-                        }}
-                    />
-                </>
-            )}
+                    }}
+                />
+            </>
         </>
     )
 }
