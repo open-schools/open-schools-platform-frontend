b1c30142cc10f2e75c560653dd678f3369f3bf37
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-08-22 20:39:00 +0500

    prettier

--- apps/schools/domains/common/components/containers/EmptyWrapper/index.tsx
+++ apps/schools/domains/common/components/containers/EmptyWrapper/index.tsx
@@ -1,31 +1,30 @@
 import React from 'react'
 import styles from './styles/styles.module.scss'
 import classNames from 'classnames'
-import {EmptyWrapperProps} from './interfaces'
+import { EmptyWrapperProps } from './interfaces'
 import Image from 'next/image'
 import duckEmptyPage from '@public/image/duckEmptyPage.svg'
-import {Typography} from 'antd'
-import {Button} from '@domains/common/components/button'
+import { Typography } from 'antd'
+import { Button } from '@domains/common/components/button'
 
 const EmptyWrapper: React.FC<EmptyWrapperProps> = ({
-                                                       isLoading,
-                                                       data,
-                                                       searchTrigger,
-                                                       pageTitle,
-                                                       titleText,
-                                                       descriptionText,
-                                                       buttonText,
-                                                       handleRunTask,
-                                                       className,
-                                                       children,
-                                                   }) => {
-
+    isLoading,
+    data,
+    searchTrigger,
+    pageTitle,
+    titleText,
+    descriptionText,
+    buttonText,
+    handleRunTask,
+    className,
+    children,
+}) => {
     if (!searchTrigger && !isLoading && (!data || data.count === 0)) {
         return (
             <div className={classNames(styles.container, className)}>
                 <Typography.Title level={1}>{pageTitle}</Typography.Title>
                 <div className={styles.duckContainer}>
-                    <Image src={duckEmptyPage} alt={'Duck with a magnifying glass'}/>
+                    <Image src={duckEmptyPage} alt={'Duck with a magnifying glass'} />
                     <div className={styles.textContainer}>
                         <Typography.Title className={styles.titleText} level={4}>
                             {titleText}
