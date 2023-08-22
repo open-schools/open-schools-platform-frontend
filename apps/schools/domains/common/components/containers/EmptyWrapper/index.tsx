6f5e6315ea5514dc4647d00e68ec8bd252bf2b07
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-08-22 15:22:39 +0500

    empty page wrapper full work

--- apps/schools/domains/common/components/containers/EmptyWrapper/index.tsx
+++ apps/schools/domains/common/components/containers/EmptyWrapper/index.tsx
@@ -1,34 +1,38 @@
 import React from "react";
 import styles from "./styles/styles.module.scss";
 import classNames from "classnames";
-import { EmptyWrapperProps } from "./interfaces";
+import {EmptyWrapperProps} from "./interfaces";
 import Image from "next/image";
 import duckEmptyPage from "@public/image/duckEmptyPage.svg";
-import { Typography } from "antd";
-import { Button } from "@domains/common/components/button";
+import {Typography} from "antd";
+import {Button} from "@domains/common/components/button";
 
 const EmptyWrapper: React.FC<EmptyWrapperProps> = (props) => {
-  const { children } = props
+    const {children} = props
 
-  if (!props.data) {
+    if (!props.isLoading && (!props.data || props.data.count === 0)) {
+        return (
+            <div className={classNames(styles.container, props.className)}>
+                <Typography.Title level={1}>{props.pageTitle}</Typography.Title>
+                <div className={styles.duckContainer}>
+                    <Image src={duckEmptyPage} alt={'Duck with a magnifying glass'}/>
+                    <div className={styles.textContainer}>
+                        <Typography.Title className={styles.titleText} level={4}>{props.titleText}</Typography.Title>
+                        <span className={styles.descriptionText}>{props.descriptionText}</span>
+                        <Button className={styles.button} onClick={props.handleRunTask}>
+                            {props.buttonText}
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
-      <div className={styles.container}>
-        <Typography.Title level={1}>{props.pageTitle}</Typography.Title>
-        <div className={classNames(styles.duckContainer, props.className)} style={props.style}>
-          <Image style={{ height: '60%', width: '100%', }} src={duckEmptyPage} alt={'Duck with a magnifying glass'}/>
-          <div>{props.titleText}</div>
-          <div>{props.descriptionText}</div>
-          <Button>{props.buttonText}</Button>
+        <div className={styles.container}>
+            {children}
         </div>
-      </div>
     );
-  }
-
-  return (
-    <div className={styles.container}>
-      {children}
-    </div>
-  );
 };
 
 export default EmptyWrapper;
--- apps/schools/domains/common/components/containers/EmptyWrapper/interfaces.ts
+++ apps/schools/domains/common/components/containers/EmptyWrapper/interfaces.ts
@@ -1,19 +1,11 @@
 import { CSSProperties, PropsWithChildren } from 'react'
 
-/**
- * Interface for props in EmptyWrapper component
- * @param style Inline styles
- * @param className ClassName for div container
- * @param data Table data
- * @param pageTitle Page title text
- * @param titleText Title text
- * @param descriptionText Description text
- * @param buttonText Button text
- */
 export interface EmptyWrapperProps extends Omit<PropsWithChildren, 'type'> {
   style?: CSSProperties
   className?: string
   data?: any
+  handleRunTask?: () => void
+  isLoading?: boolean
   pageTitle?: string
   titleText?: string
   descriptionText?: string
