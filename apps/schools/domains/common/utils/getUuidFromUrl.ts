97e91c6b71d56a880ae2af786639fa0df717ae8c
lev tarasov <Levil6.6.4@yandex.ru>
2023-09-04 16:24:06 +0500

    getUuidFromUrl fix

--- apps/schools/domains/common/utils/getUuidFromUrl.ts
+++ apps/schools/domains/common/utils/getUuidFromUrl.ts
@@ -3,7 +3,7 @@ export const getUuidFromUrl = () => {
         /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/g,
     )
 
-    if (uuidArray === null) return ''
+    if (uuidArray === null) return []
 
     return uuidArray
 }
