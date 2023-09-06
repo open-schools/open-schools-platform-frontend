1b86e46b81543c6435c6b7301de7ff6df1573180
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-09-06 17:10:44 +0500

    prettier fixes

--- apps/schools/domains/circle/components/addressForm/index.tsx
+++ apps/schools/domains/circle/components/addressForm/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import {Typography} from "antd";
-import styles from "./styles/styles.module.scss";
-import MapComponent from "@domains/circle/components/map";
+import React from 'react'
+import { Typography } from 'antd'
+import styles from './styles/styles.module.scss'
+import MapComponent from '@domains/circle/components/map'
 
 const AddressForm = () => {
-
     return (
         <div>
             <Typography.Title level={1}>Добавление кружка</Typography.Title>
-            <Typography.Title level={3}>Выберите на карте расположение кружка, для этого: выберете город, найдите нужную
-                улицу, дом и нажмите на него</Typography.Title>
-            <MapComponent/>
+            <Typography.Title level={3}>
+                Выберите на карте расположение кружка, для этого: выберете город, найдите нужную улицу, дом и нажмите на
+                него
+            </Typography.Title>
+            <MapComponent />
         </div>
-    );
-};
+    )
+}
 
-export default AddressForm;
+export default AddressForm
