6a0eb9760b8065adc0cdd7c048ba1acd2d4c8a4c
Михаил <miha061103@gmail.com>
2023-10-03 17:01:14 +0500

    added 404 page from figma

---
+++ apps/schools/domains/common/components/errors/interfaces.ts
@@ -0,0 +1,9 @@
+export interface ErrorPageProps {
+    handleRunTask?: () => void
+    titleText?: string
+    descriptionText?: string
+    buttonText?: string
+    image?: string
+    imageWidth?: number
+    imageAlt?: string
+}
--- apps/schools/pages/404.tsx
+++ apps/schools/pages/404.tsx
@@ -1,15 +1,37 @@
-import ErrorPage from 'next/error'
-import React from 'react'
-import AuthLayout from "../domains/user/components/auth/containers/AuthLayout";
+import React, { useEffect, useState } from 'react'
+import EmptyLayout from '@domains/common/components/containers/ErrorLayout'
+
+import Head from 'next/head'
+import { SharedErrorPage } from '@domains/common/components/errors'
+import router from 'next/router'
+import ducks from '@public/image/404Ducks.svg'
 
 export default function Custom404() {
+    const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        setWidth(window.screen.width * 0.6)
+    }, [])
+
+
     return (
-        <div>
-            <ErrorPage statusCode={404}/>
-        </div>
+        <>
+            <Head>
+                <title>404: Не найдено</title>
+            </Head>
+            <SharedErrorPage
+                titleText={'Страница не найдена'}
+                descriptionText={'К сожалению, страница не нашлась. Вернитесь на главную и попробуйте еще раз'}
+                buttonText={'Вернуться на главную'}
+                image={ducks}
+                imageWidth={width}
+                handleRunTask={() => {
+                    router.push('/')
+                }}
+            />
+        </>
     )
 }
 
-
-Custom404.container = AuthLayout
+Custom404.container = EmptyLayout
 Custom404.isError = true
