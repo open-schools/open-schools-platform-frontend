ebe83751a1f7ff283f861859c65864d3dc20fd17
lev tarasov <56757711+levil664@users.noreply.github.com>
2023-10-26 18:27:07 +0500

    add organization employee invite

---
+++ apps/schools/domains/common/handlers/changeStatusInvitation.ts
@@ -0,0 +1,22 @@
+import { withLoadingMessage } from '@domains/common/utils/loading'
+import { LoadingMsg } from '@domains/user/components/auth/constants/message'
+import { message } from 'antd'
+import { QueriesTypes } from '@domains/common/redux/interfaces'
+import { QueryStatuses } from '@domains/common/constants/Enums'
+
+export async function handleChangeStatusInvitation(mutation: any, id: string | undefined, status: QueriesTypes) {
+    if (id === undefined) {
+        message.error('Такой организации не существует')
+        return
+    }
+
+    let response = await withLoadingMessage(LoadingMsg, mutation, {
+        id: id,
+        status: status,
+    })
+
+    if ('data' in response)
+        status === QueryStatuses.ACCEPTED
+            ? message.success(`Вы успешно приняли заявку`)
+            : message.success(`Вы успешно отклонили заявку`)
+}
--- apps/schools/pages/_app.tsx
+++ apps/schools/pages/_app.tsx
@@ -2,16 +2,15 @@ import store from '../store/store'
 import '../domains/common/components/styles/global.scss'
 import 'antd/dist/antd.css'
 import { AppProps } from 'next/app'
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 import { Provider } from 'react-redux'
 import { AuthProvider } from '@domains/user/providers/authProvider'
 import React, { PropsWithChildren } from 'react'
 import Head from 'next/head'
-import {OrganizationProvider} from "@domains/organization/providers/organizationProvider";
-import {BaseLayout} from "@domains/common/components/containers/BaseLayoutComponents/BaseLayout";
-import {LayoutContextProvider} from "@domains/user/providers/baseLayoutProvider";
-import { message } from "antd";
-
+import { OrganizationProvider } from '@domains/organization/providers/organizationProvider'
+import { BaseLayout } from '@domains/common/components/containers/BaseLayoutComponents/BaseLayout'
+import { LayoutContextProvider } from '@domains/user/providers/baseLayoutProvider'
+import { message } from 'antd'
 
 export interface ContainerPage<PropsType> extends React.FC {
     container: React.FC<PropsType>
@@ -24,23 +23,24 @@ interface CustomAppProps extends AppProps {
 
 message.config({
     maxCount: 1,
-});
+})
 
-function MyApp ({ Component, pageProps }: CustomAppProps): JSX.Element {
+function MyApp({ Component, pageProps }: CustomAppProps): JSX.Element {
     const LayoutComponent = Component.container || BaseLayout
     const router = useRouter()
 
-    if (router.pathname === '/mobile-recaptcha') return (
-      <Provider store={store}>
-          <Head>
-              <title>Открытые школы</title>
-              <link rel="icon" href="/icons/logo.svg" sizes="any" />
-          </Head>
-          <LayoutComponent>
-              <Component {...pageProps} />
-          </LayoutComponent>
-      </Provider>
-    )
+    if (router.pathname === '/mobile-recaptcha')
+        return (
+            <Provider store={store}>
+                <Head>
+                    <title>Открытые школы</title>
+                    <link rel='icon' href='/icons/logo.svg' sizes='any' />
+                </Head>
+                <LayoutComponent>
+                    <Component {...pageProps} />
+                </LayoutComponent>
+            </Provider>
+        )
 
     return (
         <Provider store={store}>
@@ -49,7 +49,7 @@ function MyApp ({ Component, pageProps }: CustomAppProps): JSX.Element {
                     <OrganizationProvider>
                         <Head>
                             <title>Открытые школы</title>
-                            <link rel="icon" href="/icons/logo.svg" sizes="any" />
+                            <link rel='icon' href='/icons/logo.svg' sizes='any' />
                         </Head>
                         <LayoutComponent>
                             <Component {...pageProps} />
