4deaee80204c3cda749ae1d0c98c16eb2177396e
Михаил <33755295+zavarin-michael@users.noreply.github.com>
2024-06-06 19:13:25 +0500

    added yandex metrika code with env setting YANDEX_CLIENT_ID

--- apps/schools/pages/_document.tsx
+++ apps/schools/pages/_document.tsx
@@ -1,13 +1,39 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { Html, Main, NextScript, Head } from 'next/document'
 
-class CustomDocument extends Document {
-    static async getInitialProps(
-        ctx: DocumentContext
-    ): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx)
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
 
-        return initialProps
-    }
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(${process.env.YANDEX_CLIENT_ID}, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                });
+              `,
+                    }}
+                />
+                <noscript>
+                    <div>
+                        <img
+                            src={`https://mc.yandex.ru/watch/${process.env.YANDEX_CLIENT_ID}`}
+                            style={{ position: 'absolute', left: '-9999px' }}
+                            alt=''
+                        />
+                    </div>
+                </noscript>
+            </body>
+        </Html>
+    )
 }
-
-export default CustomDocument
