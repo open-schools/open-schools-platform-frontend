fa070ceef8a7a0052d24d48fa84cc85ec5265f03
nenichv <alena_volkova_2004@mail.ru>
2024-06-13 14:19:10 +0500

    fix field name

--- apps/schools/domains/user/components/auth/registerForm/index.tsx
+++ apps/schools/domains/user/components/auth/registerForm/index.tsx
@@ -11,8 +11,6 @@ import { BUTTON_FORM_GUTTER_20 } from '../constants/styles'
 import { FirebaseReCaptchaContext } from '@domains/user/providers/firebaseReCaptchaProvider'
 import { registrationHandler } from '@domains/user/handlers/auth/register'
 import { useUsersMutation } from '@domains/user/redux/userApi'
-import {PARENT_EMAIL, STUDENT_PHONE} from "@domains/student/components/createStudentForm/constants";
-import {USER_EMAIL} from "@domains/user/components/profile/profileEdit/constants";
 
 const RequiredFlagWrapper: React.FC<PropsWithChildren<any>> = (props) => {
     return <div className={styles.requiredField}>{props.children}</div>
