c629aa0f0a99a7b9d640ad748b7e62e2397f8b4b
lev tarasov <56757711+levil664@users.noreply.github.com>
2024-07-05 12:34:20 +0500

    calculateResults mappers took in transformResponse organizationApi

--- apps/schools/domains/student/handlers/resultsCalculate.ts
+++ apps/schools/domains/student/handlers/resultsCalculate.ts
@@ -6,50 +6,11 @@ export const calculateResults = (
     students: { count: number | undefined; results: any[] } | undefined,
 ): RowType[] => {
     if (paginationParams.page < Math.ceil((invites?.count ?? 0) / paginationParams.pageSize)) {
-        return (invites?.results ?? []).map((x) => {
-            return {
-                id: x.body.id,
-                student_name: x.body.name,
-                student_phone: x.additional.phone,
-                parent_phone: x.recipient.parent_phones.replaceAll(',', '\n'),
-                circle_name: x.sender.name,
-            } as RowType
-        })
+        return invites?.results ?? []
     } else if (paginationParams.page === Math.ceil((invites?.count ?? 0) / paginationParams.pageSize)) {
-        return [...(invites?.results ?? []), ...(students?.results ?? [])]
-            .slice(0, paginationParams.pageSize)
-            .map((x) => {
-                if ('body' in x) {
-                    return {
-                        id: x.body.id,
-                        student_name: x.body.name,
-                        student_phone: x.additional.phone,
-                        parent_phone: x.recipient.parent_phones.replaceAll(',', '\n'),
-                        circle_name: x.sender.name,
-                    } as RowType
-                } else {
-                    return {
-                        id: x.id,
-                        student_name: x.name,
-                        student_phone: x.student_profile.phone,
-                        parent_phone: x.student_profile.parent_phones?.replaceAll(',', '\n'),
-                        circle_name: x.circle.name,
-                    } as RowType
-                }
-            })
+        return [...(invites?.results ?? []), ...(students?.results ?? [])].slice(0, paginationParams.pageSize)
     } else {
         const temp = paginationParams.pageSize - ((invites?.count ?? 0) % paginationParams.pageSize)
-        const mapper = (x: any) =>
-            ({
-                id: x.id,
-                student_name: x.name,
-                student_phone: x.student_profile.phone,
-                parent_phone: x.student_profile.parent_phones?.replaceAll(',', '\n'),
-                circle_name: x.circle.name,
-            }) as RowType
-
-        return students?.results.length === 1
-            ? [mapper(students?.results[0])]
-            : (students?.results ?? []).slice(temp).map(mapper)
+        return students?.results.length === 1 ? [students?.results[0]] : (students?.results ?? []).slice(temp)
     }
 }
